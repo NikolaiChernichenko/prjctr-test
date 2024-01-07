d4702506186f8cb1df60538766648c9076022df6
nchernichenko <nikolai@boylesoftware.com>
2024-01-07 13:14:25 +0200

    Working on a basic layout (header, filters)

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+// SCSS stylesheets
+import '../scss/app.scss';
+
+// SVG Icons
+import '../images/svg-icons/icon-arrow.svg';
+import '../images/svg-icons/icon-heart.svg';
+import '../images/svg-icons/icon-folder.svg';
